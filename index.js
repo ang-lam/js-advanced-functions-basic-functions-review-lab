5c29e43914c8dfd2ce4f475ca09b0410c3372f84
Angela <angelalamluu@gmail.com>
2021-06-09 23:44:47 -0700

    commit tree 794fe7282a1ed11d4d9ca5dd4f2dda1e878b696c

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function saturdayFun(string="roller-skate"){
+    return `This Saturday, I want to ${string}!`
+}
+
+function mondayWork(string="go to the office") {
+    return `This Monday, I will ${string}.`
+}
+
+function wrapAdjective(string="*") {
+    return function(word="special"){
+        return `You are ${string}${word}${string}!`
+    }
+}
+
+const Calculator = {
+    add: function(a, b) {
+        return a + b
+    },
+    subtract: function(a, b) {
+        return a - b
+    },
+    multiply: function(a, b) {
+        return a * b
+    },
+    divide: function(a, b) {
+        return a / b
+    }
+}
+
+const actionApplyer = function(integer, functions) {
+    let a = integer
+    for (let i = 0; i < functions.length; i++){
+        a = functions[i](a)
+    }
+
+    return a
+}
